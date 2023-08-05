2f8dd571437133d1c15d9d54002dde9ea841e5ac
KincaidYang <91786638+KincaidYang@users.noreply.github.com>
2023-08-05 21:39:34 +0800

    Update config.go

--- DNSPod-Go/config.go
+++ DNSPod-Go/config.go
@@ -1,13 +1,22 @@
 package main
 
 const (
-	domain     = "r2wind.cn"
-	subdomain  = "@"
-	CDNCNAME   = "r2wind.cn.eo.dnse3.com"
-	Token      = ""
-	SecretId   = ""
-	SecretKey  = ""
-	DoH        = "https://119.29.29.99/d"
+	// 请替换成您自己的域名
+	domain = "r2wind.cn"
+	// 请替换成您自己的子域名前缀，如果不使用子域名请写 @
+	subdomain = "ipv6"
+	// 请替换成您的 CDN或其他服务的 CNAME 服务地址
+	// 使用前请前往 https://console.cloud.tencent.com/httpdns/domain 将 CDN 服务域名添加到 HTTPDNS
+	// 仅需添加主域名，如您的 CDN 服务域名为 cdn.r2wind.cn，仅需填写 r2wind.cn 完成添加即可
+	CDNCNAME = "r2wind.cn.eo.dnse3.com"
+	// 请替换成您的腾讯云HTTPDNS服务的密钥，可前往 https://console.cloud.tencent.com/httpdns/configure 获取
+	Token     = ""
+	SecretId  = ""
+	SecretKey = ""
+	// 注意，该地址为DNSPod HTTPDNS服务地址，无需更换
+	DoH = "https://119.29.29.99/d"
+	// 记录类型，AAAA为IPv6记录，A为IPv4记录
 	recordType = "AAAA"
-	recordTTL  = 60
+	// 记录TTL，单位秒，建议不低于60秒
+	recordTTL = 60
 )
