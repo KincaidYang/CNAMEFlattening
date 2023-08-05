369ac53506a79e78854f73bc5cb56d40d2f5a232
KincaidYang <91786638+KincaidYang@users.noreply.github.com>
2023-08-05 19:32:22 +0800

    Update go
    新增go语言版本

---
+++ DNSPod-Go/config.go
@@ -0,0 +1,13 @@
+package main
+
+const (
+	domain     = "r2wind.cn"
+	subdomain  = "@"
+	CDNCNAME   = "r2wind.cn.eo.dnse3.com"
+	Token      = ""
+	SecretId   = ""
+	SecretKey  = ""
+	DoH        = "https://119.29.29.99/d"
+	recordType = "AAAA"
+	recordTTL  = 60
+)
---
+++ DNSPod-Go/go.mod
@@ -0,0 +1,8 @@
+module github.com/KincaidYang/CNAMEFlattening/DNSPod-Go
+
+go 1.20
+
+require (
+	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common v1.0.715
+	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/dnspod v1.0.715
+)
