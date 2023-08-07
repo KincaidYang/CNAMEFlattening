630aa304e6cf948296023f4bfe6cde8f707b3cb4
KincaidYang <91786638+KincaidYang@users.noreply.github.com>
2023-08-07 21:48:56 +0800

    线路完善
    完善了教育网、铁通等线路

--- go/DNSPod/Storm/cilent.go
+++ go/DNSPod/Storm/cilent.go
@@ -43,6 +43,14 @@ func main() {
 		"重庆联通", "重庆电信", "重庆移动", "四川联通", "四川电信", "四川移动", "贵州联通", "贵州电信", "贵州移动", "云南联通", "云南电信", "云南移动", "西藏联通", "西藏电信", "西藏移动",
 		// 西北地区
 		"陕西联通", "陕西电信", "陕西移动", "甘肃联通", "甘肃电信", "甘肃移动", "青海联通", "青海电信", "青海移动", "宁夏联通", "宁夏电信", "宁夏移动", "新疆联通", "新疆电信", "新疆移动",
+		// 铁通
+		"辽宁铁通", "吉林铁通", "黑龙江铁通", "北京铁通", "河北铁通", "山西铁通", "内蒙铁通", "天津铁通", "上海铁通", "江苏铁通",
+		"浙江铁通", "安徽铁通", "福建铁通", "江西铁通", "山东铁通", "河南铁通", "湖北铁通", "湖南铁通", "广东铁通", "广西铁通",
+		"海南铁通", "重庆铁通", "四川铁通", "贵州铁通", "云南铁通", "西藏铁通", "陕西铁通", "甘肃铁通", "青海铁通", "宁夏铁通", "新疆铁通",
+		// 教育网
+		"辽宁教育网", "吉林教育网", "黑龙江教育网", "北京教育网", "河北教育网", "山西教育网", "内蒙教育网", "天津教育网", "上海教育网", "江苏教育网",
+		"浙江教育网", "安徽教育网", "福建教育网", "江西教育网", "山东教育网", "河南教育网", "湖北教育网", "湖南教育网", "广东教育网", "广西教育网",
+		"海南教育网", "重庆教育网", "四川教育网", "贵州教育网", "云南教育网", "西藏教育网", "陕西教育网", "甘肃教育网", "青海教育网", "宁夏教育网", "新疆教育网",
 		// 默认
 		"默认",
 		// 如需覆盖更多线路请在此处继续添加，并在dnsIPs中添加 LocalDNS IP 所对应的变量名称
@@ -57,6 +65,15 @@ func main() {
 		DNS_GUANGDONG_CU, DNS_GUANGDONG_CT, DNS_GUANGDONG_CM, DNS_GUANGXI_CU, DNS_GUANGXI_CT, DNS_GUANGXI_CM, DNS_HAINAN_CU, DNS_HAINAN_CT, DNS_HAINAN_CM,
 		DNS_CHONGQING_CU, DNS_CHONGQING_CT, DNS_CHONGQING_CM, DNS_SICHUAN_CU, DNS_SICHUAN_CT, DNS_SICHUAN_CM, DNS_GUIZHOU_CU, DNS_GUIZHOU_CT, DNS_GUIZHOU_CM, DNS_YUNNAN_CU, DNS_YUNNAN_CT, DNS_YUNNAN_CM, DNS_XIZANG_CU, DNS_XIZANG_CT, DNS_XIZANG_CM,
 		DNS_SHAANXI_CU, DNS_SHAANXI_CT, DNS_SHAANXI_CM, DNS_GANSU_CU, DNS_GANSU_CT, DNS_GANSU_CM, DNS_QINGHAI_CU, DNS_QINGHAI_CT, DNS_QINGHAI_CM, DNS_NINGXIA_CU, DNS_NINGXIA_CT, DNS_NINGXIA_CM, DNS_XINJIANG_CU, DNS_XINJIANG_CT, DNS_XINJIANG_CM,
+		// 铁通
+		DNS_LIAONING_CTT, DNS_JILIN_CTT, DNS_HEILONGJIANG_CTT, DNS_BEIJING_CTT, DNS_HEBEI_CTT, DNS_SHANXI_CTT, DNS_NEIMENGGU_CTT, DNS_TIANJIN_CTT, DNS_SHANGHAI_CTT, DNS_JIANGSU_CTT,
+		DNS_ZHEJIANG_CTT, DNS_ANHUI_CTT, DNS_FUJIAN_CTT, DNS_JIANGXI_CTT, DNS_SHANDONG_CTT, DNS_HENAN_CTT, DNS_HUBEI_CTT, DNS_HUNAN_CTT, DNS_GUANGDONG_CTT, DNS_GUANGXI_CTT,
+		DNS_HAINAN_CTT, DNS_CHONGQING_CTT, DNS_SICHUAN_CTT, DNS_GUIZHOU_CTT, DNS_YUNNAN_CTT, DNS_XIZANG_CTT, DNS_SHAANXI_CTT, DNS_GANSU_CTT, DNS_QINGHAI_CTT, DNS_NINGXIA_CTT,
+		// 教育网
+		DNS_LIAONING_EDU, DNS_JILIN_EDU, DNS_HEILONGJIANG_EDU, DNS_BEIJING_EDU, DNS_HEBEI_EDU, DNS_SHANXI_EDU, DNS_NEIMENGGU_EDU, DNS_TIANJIN_EDU, DNS_SHANGHAI_EDU, DNS_JIANGSU_EDU,
+		DNS_ZHEJIANG_EDU, DNS_ANHUI_EDU, DNS_FUJIAN_EDU, DNS_JIANGXI_EDU, DNS_SHANDONG_EDU, DNS_HENAN_EDU, DNS_HUBEI_EDU, DNS_HUNAN_EDU, DNS_GUANGDONG_EDU, DNS_GUANGXI_EDU,
+		DNS_HAINAN_EDU, DNS_CHONGQING_EDU, DNS_SICHUAN_EDU, DNS_GUIZHOU_EDU, DNS_YUNNAN_EDU, DNS_XIZANG_EDU, DNS_SHAANXI_EDU, DNS_GANSU_EDU, DNS_QINGHAI_EDU, DNS_NINGXIA_EDU,
+		// 默认
 		DNS_SHANGHAI_CT,
 		// 添加前请前往localdns.go中添加对应的变量
 	}
